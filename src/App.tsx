93908bf0f864f7509ba0986e6f63f9ee304de330
christopher.ivey <christopher.ivey1@snhu.edu>
2023-06-17 20:17:18 -0400

    init

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
+
+function App() {
+  const [alertVisible, setAlertVisibility] = useState(false);
+  return (
+    <div>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>My alert</Alert>
+      )}
+      <Button onClick={() => setAlertVisibility(true)}>My Button</Button>
+    </div>
+  );
+}
+
+export default App;
